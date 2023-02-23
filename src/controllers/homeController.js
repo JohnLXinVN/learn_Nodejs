129dc430ff7730788868794a4e754dc961b8030b
anhhm-john <anhhm.2003.work@gmail.com>
2023-02-23 10:25:12 +0700

    #21 ket noi db with sequelize

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,15 @@
+const db = require("../models")
 
-const getHomePage = (req, res) => {
+const getHomePage = async (req, res) => {
+    
+    try {
+        let data = await db.User.findAll();
+        return res.render('homepage.ejs', {
+            data
+        })
+    } catch (e) {
+        console.log(e)
+    }
     return res.render('homepage.ejs')
 }
 
